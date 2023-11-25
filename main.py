d951834fab0cd509e27e36ce487cb4473cb32f59
Daryl V <daryl.villalobos@hotmail.com>
2023-11-25 11:04:38 +0900

    remove extra spaces in JA

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import PyPDF2
+import re
+import os
 
 def extract_text_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as file:
@@ -10,13 +12,19 @@ def extract_text_from_pdf(pdf_path):
             text += "\n----------------------------------------\n"
         return text
 
-def save_text_to_file(text, output_path):
+def clean_japanese_text(text):
+    # Regular expression pattern to identify and remove spaces between Japanese characters
+    japanese_char_pattern = r'(?<=[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\u3400-\u4dbf\u4e00-\u9fff]) ' \
+                            r'(?=[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\u3400-\u4dbf\u4e00-\u9fff])'
+    return re.sub(japanese_char_pattern, '', text)
+
+def save_text_to_file(text, pdf_path):
+    base_name = os.path.splitext(os.path.basename(pdf_path))[0]
+    output_path = f'{base_name}_output_text.txt'
     with open(output_path, 'w', encoding='utf-8') as file:
         file.write(text)
 
 file_path = "input_files/SR21_J_All.pdf"
-# Replace 'your_pdf_file.pdf' with the path to your PDF file
-pdf_text = extract_text_from_pdf(file_path)
-
-# Save the extracted text to a text file
-save_text_to_file(pdf_text, 'output_text_file.txt')
+extracted_text = extract_text_from_pdf(file_path)
+cleaned_text = clean_japanese_text(extracted_text)
+save_text_to_file(cleaned_text, file_path)
